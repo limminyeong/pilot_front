b462ca7332fe5c00c4e43d5d8dee6f1904bb3fd5
임민영 <lim.minyeong.hcg@gmail.com>
2020-01-21 12:57:21 +0900

    page naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import { Header } from './components/Header';
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      {/* <Main /> */}
+      <Router>
+        <Header />
+        <Route path="/" component={Landing}/>
+      </Router>
     </div>
   );
 }
